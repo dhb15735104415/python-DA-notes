1498371554f0131cbcd02ba9fdc362ce8cdf9f45
duanhaobin <1807227469@qq,com>
2019-12-05 10:45:20 +0800

    'bilibili弹幕数据采集练习\Selenium框架学习'

---
+++ selenium/codes/test_driver.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2019/12/5 10:00
+# @Author  : duanhaobin
+# @File    : test_driver.py
+# @Software: PyCharm
+# @desc:
+
+
---
+++ spider/program/bilibili_danmu_data_collection.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2019/12/5 7:58
+# @Author  : duanhaobin
+# @File    : bilili_danmu_data_collection.py
+# @Software: PyCharm
+# @desc:   Bilibli弹幕采集
+
+
